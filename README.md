f0fd2def78a544e48255fc38d565584414912a7c
Dean Shi <dean.xiaoshi@gmail.com>
2018-02-07 23:47:17 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Quadrigacx cli
+[![npm version](https://badge.fury.io/js/quadrigacx-cli.svg)](https://www.npmjs.com/package/quadrigacx-cli)
+[![npm](https://img.shields.io/npm/dt/quadrigacx-cli.svg)](https://www.npmjs.com/package/quadrigacx-cli)
+[![dependencies Status](https://david-dm.org/dnshi/quadrigacx-cli/status.svg)](https://david-dm.org/dnshi/quadrigacx-cli)
+
+Command line tool to fetch BTC, ETH, and LTC price.
+
+## Install
+```sh
+$ npm i -g quadrigacx-cli
+$ cx
+```
+
+## Screenshot
+![screenshot](./screenshot.png?raw=true)
---
+++ src/index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+/**
+ * Author: Dean Shi <dean.xiaoshi@gmail.com>
+ * Date:   2018-02-09
+ */
+
+require('colors')
+
+const r2 = require('r2')
+const Table = require('cli-table2')
+const program = require('commander')
+
+const getBookUrl = book => `https://api.quadrigacx.com/v2/ticker?book=${book}`
+const getCurrentTradingInformation = book => r2(getBookUrl(book)).json
+const books = [
+  'btc_cad',
+  'eth_cad',
+  'ltc_cad',
+]
+
+program
+  .version(require('../package.json').version)
+  .parse(process.argv)
+
+init()
+
+async function init() {
+  try {
+    const results = await Promise.all(books.map(getCurrentTradingInformation))
+    const table = new Table({
+      head: ['Coin', 'Price CAD', 'Buy Price', 'Sell Price', 'High', 'Low'].map(title => title.yellow),
+    })
+
+    table.push(...results.map(({
+      last, bid, ask, high, low,
+    }, i) => [books[i].replace(/_cad/g, '').toUpperCase(), last, bid, ask, high, low]))
+
+    console.log()
+    console.log(table.toString())
+    console.log(`Data source from quadrigacx.com at ${new Date().toLocaleString().split(', ')[1]}`)
+  } catch (error) {
+    console.error('⚠️  Cannot fetch data'.bold.red)
+    console.log(error)
+  }
+}
